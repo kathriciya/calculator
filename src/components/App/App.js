4e686eb17b4e94041b5a0426238eef04ab952ece
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-03 06:09:11 +0700

    create config

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './App.module.scss';
+const App = () => {
+  return (
+    <div className={s.red}>
+      <h1>Test1</h1>
+      <h2>Test2</h2>
+    </div>
+  );
+};
+export default App;
