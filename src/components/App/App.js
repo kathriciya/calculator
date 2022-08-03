c05f8f0731701f163bf04ee8ed445bfcb2807837
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-03 08:11:26 +0700

    add components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Button from '../Button';
+import ButtonWrap from '../ButtonWrap';
+import CalculatorWrap from '../Calculator';
+import Display from '../Display';
 import s from './App.module.scss';
+
+const buttons = [
+  ['C', 0, '.', '/'],
+  [7, 8, 9, 'x'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  ['='],
+];
 const App = () => {
   return (
-    <div className={s.red}>
-      <h1>Test1</h1>
-      <h2>Test2</h2>
+    <div className={s.container}>
+      <CalculatorWrap>
+        <Display />
+        <ButtonWrap>
+          {buttons.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                value={btn}
+                onClick={() => console.log('click')}
+              />
+            );
+          })}
+        </ButtonWrap>
+      </CalculatorWrap>
     </div>
   );
 };
