65c4a1ca0ecf5809821ba3fda280475e1ada750d
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-03 08:10:17 +0700

    create Display

---
+++ src/components/Display/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './style.module.scss';
+
+const Display = () => {
+  return (
+    <div className={s.display}>
+      <span className={s.result}>0</span>
+    </div>
+  );
+};
+
+export default Display;
